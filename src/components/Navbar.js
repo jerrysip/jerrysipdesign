4f15296917dad32c9ff719d8d4e65c3f8745c775
Jerry <jerrsiphavong@icloud.com>
2021-05-07 23:52:34 -0400

    testing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,25 +13,27 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo">JERRYSIP</h1>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <div className="nav-wrap">
+        <nav className="NavbarItems">
+          <h1 className="navbar-logo">JERRYSIP</h1>
+          <div className="menu-icon" onClick={this.handleClick}>
+            <i
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+          <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a className={item.cName} href={item.url}>
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
